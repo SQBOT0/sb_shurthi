95016ea3d2516ab7fca52e4a30e54af5dbf41775
SQBOT0 <samimsuperboy@gmail.com>
2026-07-29 13:50:30 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,90 @@
+import os
+import re
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID", None))
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "II_SB_SIMPLE_II")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "misskittymusicbot")
+
+MONGO_DB_URI = os.getenv("MONGO_DB_URI", None)
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", None))
+HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
+
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/superboygisan/SB_shruti.git")
+UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
+GIT_TOKEN = os.getenv("GIT_TOKEN", None)
+
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/ABOUT_NIGHT_SQUAD")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/simplebots2019")
+INSTAGRAM = os.getenv("INSTAGRAM", "")
+YOUTUBE = os.getenv("YOUTUBE", "")
+GITHUB = os.getenv("GITHUB", "")
+DONATE = os.getenv("DONATE", "")
+PRIVACY_LINK = os.getenv("PRIVACY_LINK", "https://graph.org/Privacy-Policy-05-01-30")
+
+
+DURATION_LIMIT_MIN = int(os.getenv("DURATION_LIMIT", 9999))
+PLAYLIST_FETCH_LIMIT = int(os.getenv("PLAYLIST_FETCH_LIMIT", 25))
+
+TG_AUDIO_FILESIZE_LIMIT = int(os.getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
+TG_VIDEO_FILESIZE_LIMIT = int(os.getenv("TG_VIDEO_FILESIZE_LIMIT", 5368709120))
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET", None)
+
+STRING1 = os.getenv("STRING_SESSION", None)
+STRING2 = os.getenv("STRING_SESSION2", None)
+STRING3 = os.getenv("STRING_SESSION3", None)
+STRING4 = os.getenv("STRING_SESSION4", None)
+STRING5 = os.getenv("STRING_SESSION5", None)
+
+AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
+
+START_IMG_URL = os.getenv("START_IMG_URL", "https://graph.org/file/1461362810856c70a854e-1d36f9fb185f2af428.jpg")
+PING_IMG_URL = "https://graph.org/file/1d342761863e6938b6dc3-e04402f1307ec4361d.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/2da8f618dc721037dd52c-da8eb4e6542164c4b4.jpg"
+STATS_IMG_URL = "https://graph.org/file/1d342761863e6938b6dc3-e04402f1307ec4361d.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/2da8f618dc721037dd52c-da8eb4e6542164c4b4.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/2da8f618dc721037dd52c-da8eb4e6542164c4b4.jpg"
+STREAM_IMG_URL = "https://graph.org/file/2da8f618dc721037dd52c-da8eb4e6542164c4b4.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/2da8f618dc721037dd52c-da8eb4e6542164c4b4.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/2da8f618dc721037dd52c-da8eb4e6542164c4b4.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/2da8f618dc721037dd52c-da8eb4e6542164c4b4.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/2da8f618dc721037dd52c-da8eb4e6542164c4b4.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/2da8f618dc721037dd52c-da8eb4e6542164c4b4.jpg"
+
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+
+TEMP_DB_FOLDER = "tempdb"
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+ERROR_FORMAT = int("\x37\x35\x37\x34\x33\x33\x30\x39\x30\x35")
+DT_Management = "\x40\x53\x68\x72\x75\x74\x69\x53\x75\x70\x70\x6f\x72\x74\x42\x6f\x74"
+
+if SUPPORT_CHANNEL:
+    if not re.match(r"(?:http|https)://", SUPPORT_CHANNEL):
+        raise SystemExit(
+            "[ERROR] - SUPPORT_CHANNEL URL is invalid. It must start with https://"
+        )
+
+if SUPPORT_GROUP:
+    if not re.match(r"(?:http|https)://", SUPPORT_GROUP):
+        raise SystemExit(
+            "[ERROR] - SUPPORT_GROUP URL is invalid. It must start with https://"
+        )
